6ca755ebf04c13c1b8225e98c505e2d9ec8fb2ac
Bryce <egymlol@gmail.com>
2022-08-07 19:40:39 +1000

    - Changed player animations to scale / rotate sprite only - That fixed pickup radius expanding

--- Assets/Project/Game/Runtime/Scripts/Entities/Player/PlayerPickup.cs
+++ Assets/Project/Game/Runtime/Scripts/Entities/Player/PlayerPickup.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 [ExecuteInEditMode]
 public class PlayerPickup : MonoBehaviour
 {
     private PlayerBase playerBase;
+    private Light2D pickupRing;
+    private CircleCollider2D pickupCollider;
 
     private void Awake()
     {
         playerBase = GetComponentInParent<PlayerBase>();
+        pickupCollider = GetComponent<CircleCollider2D>();
+        pickupRing = GetComponent<Light2D>();
     }
 
     private void FixedUpdate()
     {
         float radius = playerBase.StatsAsset.PickupRadius;
-        transform.localScale = new Vector3(radius, radius, 0);
+
+        pickupCollider.radius = radius / 2;
+
+        pickupRing.pointLightInnerRadius = (radius * .75f) / 2;
+        pickupRing.pointLightOuterRadius = radius / 2;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
